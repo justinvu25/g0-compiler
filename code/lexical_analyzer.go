00d4a3163142e3691bfd8054b64e27efb6173632
spicyChick3ns <gonzalesmsd@gmail.com>
2020-03-21 17:53:32 -0400

    eat comments fix

--- code/lexical_analyzer.go
+++ code/lexical_analyzer.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"regexp"
-	"sync"
-	"strings"
-	"unicode"
-	"strconv"
 	"math"
+	"strconv"
+	"strings"
+	"sync"
+	"unicode"
 )
 
 //Get data from the specified file and attach and EOF character (~) onto it.
@@ -17,42 +16,42 @@ import (
 //produce the appropriate tokens.
 
 type inputData struct {
-	input      	string // P0 source code
-	sym			int // Symbol that was identified.
-	ch			string // Current character.
-	index		int // Helps identify symbols
-	val			string // Value
-	lineNumber 	int // Current line being parsed
-	lastLine	int // Previous line that was parsed
-	errorLine	int // Used to help surpress multiple errors
-	pos 		int // Current position of parser in a line
-	lastPos		int // Previous position
-	errorPos	int // Used to help surpress multiple errors
-	error      	bool // Set to true when an error is found.
+	input      string // P0 source code
+	sym        int    // Symbol that was identified.
+	ch         string // Current character.
+	index      int    // Helps identify symbols
+	val        string // Value
+	lineNumber int    // Current line being parsed
+	lastLine   int    // Previous line that was parsed
+	errorLine  int    // Used to help surpress multiple errors
+	pos        int    // Current position of parser in a line
+	lastPos    int    // Previous position
+	errorPos   int    // Used to help surpress multiple errors
+	error      bool   // Set to true when an error is found.
 }
 
 // constructor for inputData struct
 func newInputData(fileName string) *inputData {
 	input := fileHelper(fileName)
 	s := inputData{
-		input: input, 
-		sym: 0,
-		ch: "",
-		index: 0,
-		val: "",
+		input:      input,
+		sym:        0,
+		ch:         "",
+		index:      0,
+		val:        "",
 		lineNumber: 1,
-		lastLine: 1,
-		errorLine: 1,
-		pos: 0,
-		lastPos: 0,
-		errorPos: 0, 
-		error: false}
+		lastLine:   1,
+		errorLine:  1,
+		pos:        0,
+		lastPos:    0,
+		errorPos:   0,
+		error:      false}
 	return &s
 }
 
 func main() {
 	var wg sync.WaitGroup
-	inputData := newInputData("p0test.txt")//newInputData("p0code.txt")
+	inputData := newInputData("p0test.txt") //newInputData("p0code.txt")
 
 	// Add items to the wait group, one for each goroutine.
 	wg.Add(3)
@@ -88,18 +87,19 @@ func parseInput(inputData *inputData, wg *sync.WaitGroup) {
 	for inputData.ch != "~" {
 		GetSym(inputData)
 	}
-	
+
 	fmt.Println("done parsing")
 }
 
 // Removes whitespace.
 func eatWhitespace(inputData *inputData, wg *sync.WaitGroup) {
 	defer wg.Done()
-	// 
+	//
 	inputData.input = strings.Replace(inputData.input, " do", "!do", -1)
 	inputData.input = strings.Replace(inputData.input, " end", "!end", -1)
 	// Giving -1 to string.Replace removes an unlimited number of whitespaces.
 	inputData.input = strings.Replace(inputData.input, " ", "", -1)
+
 	fmt.Println("done removing whitespace")
 }
 
@@ -112,8 +112,29 @@ func eatWhitespace(inputData *inputData, wg *sync.WaitGroup) {
 
 func eatComments(inputData *inputData, wg *sync.WaitGroup) {
 	defer wg.Done()
-	re := regexp.MustCompile(`(?s)\{[^~.]*\}`)
-	inputData.input = re.ReplaceAllString(inputData.input, "")
+	i := 0
+	opening := false
+	closing := false
+	m := 0
+	n := 0
+
+	for string((inputData.input)[i]) != "~" {
+		if string((inputData.input)[i]) == "{" {
+			m = i
+			opening = true
+		} else if string((inputData.input)[i]) == "}" && opening == true {
+			n = i
+			closing = true
+		}
+		i++
+		if opening == true && closing == true {
+			opening = false
+			closing = false
+			inputData.input = string((inputData.input)[:m] + "" + (inputData.input)[n+1:])
+			i = m
+		}
+	}
+	// fmt.Println(inputData.input)
 	fmt.Println("done removing comments")
 }
 
@@ -123,71 +144,70 @@ func eatComments(inputData *inputData, wg *sync.WaitGroup) {
 
 */
 const (
-	TIMES = 1
-	DIV = 2
-	MOD = 3
-	AND = 4
-	PLUS = 5
-	MINUS = 6
-	OR = 7
-	EQ = 8
-	NE = 9
-	LT = 10
-	GT = 11
-	LE = 12
-	GE = 13
-	PERIOD = 14
-	COMMA = 15
-	COLON = 16
-	RPAREN = 17
-	RBRAK = 18
-	OF = 19
-	THEN = 20
-	DO = 21
-	LPAREN = 22
-	LBRAK = 23
-	NOT = 24
-	BECOMES = 25
-	NUMBER = 26
-	IDENT = 27
+	TIMES     = 1
+	DIV       = 2
+	MOD       = 3
+	AND       = 4
+	PLUS      = 5
+	MINUS     = 6
+	OR        = 7
+	EQ        = 8
+	NE        = 9
+	LT        = 10
+	GT        = 11
+	LE        = 12
+	GE        = 13
+	PERIOD    = 14
+	COMMA     = 15
+	COLON     = 16
+	RPAREN    = 17
+	RBRAK     = 18
+	OF        = 19
+	THEN      = 20
+	DO        = 21
+	LPAREN    = 22
+	LBRAK     = 23
+	NOT       = 24
+	BECOMES   = 25
+	NUMBER    = 26
+	IDENT     = 27
 	SEMICOLON = 28
-	END = 29
-	ELSE = 30
-	IF = 31
-	WHILE = 32
-	ARRAY = 33
-	RECORD = 34
-	CONST = 35
-	TYPE = 36
-	VAR = 37
+	END       = 29
+	ELSE      = 30
+	IF        = 31
+	WHILE     = 32
+	ARRAY     = 33
+	RECORD    = 34
+	CONST     = 35
+	TYPE      = 36
+	VAR       = 37
 	PROCEDURE = 38
-	BEGIN = 39
-	PROGRAM = 40
-	EOF = 41
+	BEGIN     = 39
+	PROGRAM   = 40
+	EOF       = 41
 )
 
 var Keywords = map[string]int{
-	"div":			DIV, 
-	"mod":			MOD, 
-	"and":			AND, 
-	"or":			OR, 
-	"of":			OF, 
-	"then":			THEN,
-	"do":			DO, 
-	"not":			NOT, 
-	"end":			END, 
-	"else":			ELSE, 
-	"if":			IF, 
-	"while":		WHILE,
-	"array":		ARRAY,
-	"record":		RECORD, 
-	"const":		CONST, 
-	"type":   		TYPE,
-	"var":			VAR, 
-	"procedure":	PROCEDURE, 
-	"begin":		BEGIN, 
-	"program": 		PROGRAM}
-
+	"div":       DIV,
+	"mod":       MOD,
+	"and":       AND,
+	"or":        OR,
+	"of":        OF,
+	"then":      THEN,
+	"do":        DO,
+	"not":       NOT,
+	"end":       END,
+	"else":      ELSE,
+	"if":        IF,
+	"while":     WHILE,
+	"array":     ARRAY,
+	"record":    RECORD,
+	"const":     CONST,
+	"type":      TYPE,
+	"var":       VAR,
+	"procedure": PROCEDURE,
+	"begin":     BEGIN,
+	"program":   PROGRAM}
 
 //
 //
@@ -196,7 +216,6 @@ func Init(inputData *inputData) {
 	GetChar(inputData)
 	GetSym(inputData)
 }
-
 
 //
 // Moves the index to the next char
@@ -211,7 +230,7 @@ func GetChar(inputData *inputData) {
 		inputData.ch = currentChar
 	} else {
 		inputData.ch = currentChar
-		inputData.index += 1 
+		inputData.index += 1
 		if currentChar == "\n" {
 			inputData.pos = 0
 			inputData.lineNumber += 1
@@ -255,7 +274,7 @@ func Number(inputData *inputData) {
 	}
 
 	res, err3 := strconv.Atoi(inputData.val)
-	if float64(res) >= math.Exp2(31){
+	if float64(res) >= math.Exp2(31) {
 		PrintError(inputData, "number too large")
 	}
 	if err3 != nil {
